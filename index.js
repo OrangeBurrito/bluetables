538f624d9784396afabc2c046335e310eebae991
orangeburrito <mali.kewl.pool@gmail.com>
2020-02-13 14:28:57 +0700

    JERJ

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector('.add')
 const list = document.querySelector('.todos')
+const search = document.querySelector('.search input')
 
 const generateTemplate = todo => {
 	const html = `
@@ -7,13 +8,12 @@ const generateTemplate = todo => {
 		<span>${todo}</span>
 		<i class="far fa-trash-alt delete"></i>
 	</li>
-	`;
+	`
 
 	list.innerHTML += html
 }
 
 addForm.addEventListener('submit', e => {
-
 	e.preventDefault()
 	const todo = addForm.add.value.trim()
 
@@ -30,10 +30,14 @@ list.addEventListener('click', e => {
 })
 
 const filterTodos = (term) => {
+	
 	Array.from(list.children)
-	.filter(() => {
+	.filter((todo) => !todo.textContent.toLowerCase().includes(term))
+	.forEach((todo) => todo.classList.add('filtered'))
 
-	})
+	Array.from(list.children)
+	.filter((todo) => todo.textContent.includes(term))
+	.forEach((todo) => todo.classList.remove('filtered'))
 }
 
 search.addEventListener('keyup', () => {
